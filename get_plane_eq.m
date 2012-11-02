05b640d6d0df70f0febc3a81d365433c79b136fb
Tomas <tomniscientia@gmail.com>
2012-11-02 04:14:05 -0500

    moved from x y z to a b c

--- get_plane_eq.m
+++ get_plane_eq.m
@@ -1,6 +1,6 @@
 % given three points on the plane,
 % represent a cut as a normal plane equation
-% in the form [x,y,z,d]
+% in the form [a, b, c, d] 
 function [eq] = get_plane_eq(p1, p2, p3)
 	nrm = get_normal(p1, p2, p3);
 	d = dot(nrm, p1);
