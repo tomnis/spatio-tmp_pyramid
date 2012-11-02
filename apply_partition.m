4bb2e145e5a2aa7053192dd1b4678b79d66c2bd1
Tomas <tomniscientia@gmail.com>
2012-11-02 00:21:14 -0500

    added apply_partition, which transforms from fractional representations of planar cuts to concrete representations that are tied to the dimensions of a specific clip. next, change compute_hist to reflect this

---
+++ apply_partition.m
@@ -0,0 +1,36 @@
+% given: 
+%
+% a partition scheme (fractional representation of cuts)
+% parameters representing the dimensions of the clip in question
+%
+% return:
+% equations of the form [a b c d] representing the cut planes
+function [cut_eqs] = apply_partition(partition, xlen, ylen, start_frame, end_frame)
+	
+	zlen = end_frame - start_frame + 1;
+
+	% for each level in the partition scheme
+	for lvl = 1:length(partition)
+		cut_eqs(lvl) = struct('xcuts', [], 'ycuts', [], 'zcuts', []);
+
+		% for each cut in the current level
+		for i = 1:size(partition(lvl).xcut_fracs, 1)
+			% multiplying the fractional cut vectors by the x and y length
+			% will yield, respectively, the correct cut boundaries in the clip are partitioning
+			xcut = partition(lvl).xcut_fracs(i,:)	* xlen;
+			ycut = partition(lvl).ycut_fracs(i,:)	* ylen;
+			% we must multiply the fractional cuts along the temporal dimension by the total number of frames
+			zcut = [start_frame start_frame 0] + partition(lvl).zcut_fracs(i,:) * zlen;
+
+			% get three points that lie on the plane
+			[p1 p2 p3] = get_points(xcut, xlen, ylen, start_frame, end_frame);
+			% use them to compute a plane equation
+			cut_eqs(lvl).xcuts = [cut_eqs(lvl).xcuts; get_plane_eq(p1, p2, p3)];			
+
+			[p1 p2 p3] = get_points(ycut, xlen, ylen, start_frame, end_frame);
+			cut_eqs(lvl).ycuts = [cut_eqs(lvl).ycuts; get_plane_eq(p1, p2, p3)];			
+			[p1 p2 p3] = get_points(zcut, xlen, ylen, start_frame, end_frame);
+			cut_eqs(lvl).zcuts = [cut_eqs(lvl).zcuts; get_plane_eq(p1, p2, p3)];			
+		end
+	end
+end
