21e8c9fb7ec2e721d0f4da99de913720d32dafdf
Tomas <tomniscientia@gmail.com>
2012-11-01 22:22:03 -0500

    changed main.m to have {k} indexing instead of {1,k} indexing

--- main.m
+++ main.m
@@ -17,15 +17,15 @@ path3 = [path0 'ADL_detected_objects/testset/passive/'];  %% detected passive ob
 
 %[data best_s_active best_s_passive locations_active locations_passive frs objects_active objects_passive] = read_data(person_ids, path1, path2, path3);
 
-
+% we have the loaded object data saved in this file
 %save tempfile
 load tempfile
 
 
 object_type = 'active_passive'
 %object_type = 'passive'
-nlevels = 1
-protate = 0.5
+nlevels = 2
+protate = 0.8
 dim = struct('start_frame', 1, 'end_frame', 1000, 'xlen', 1280, 'ylen', 960, 'protate', protate);
 
 
@@ -38,26 +38,26 @@ for k = 1:length(data.label)
   %f1
   %size(f1)
 	% the frames of person i that are in this clip
-  data.frs{1, k} = frs{i}(f1);
+  data.frs{k} = frs{i}(f1);
 				 
-  data.best_s_active{1, k} = best_s_active{i}(:, f1);
-  data.best_s_active{1, k} = data.best_s_active{1, k} + 0.7;
-  data.best_s_active{1, k}(data.best_s_active{1, k} < 0) = 0;
+  data.best_s_active{k} = best_s_active{i}(:, f1);
+  data.best_s_active{k} = data.best_s_active{k} + 0.7;
+  data.best_s_active{k}(data.best_s_active{k} < 0) = 0;
   
-	data.best_s_passive{1, k} = best_s_passive{i}(:, f1);
-  data.best_s_passive{1, k} = data.best_s_passive{1, k} + 0.35;
-  data.best_s_passive{1, k}(data.best_s_passive{1, k} < 0) = 0;
+	data.best_s_passive{k} = best_s_passive{i}(:, f1);
+  data.best_s_passive{k} = data.best_s_passive{k} + 0.35;
+  data.best_s_passive{k}(data.best_s_passive{k} < 0) = 0;
   
 	if isequal(object_type, 'active_passive')
-    data.best_s{1, k} = [data.best_s_active{1, k}; data.best_s_passive{1, k}];  %% active + passive objects
-		data.locs{1, k} = [locations_active{i}(:, f1, :); locations_passive{i}(:, f1, :)];
+    data.best_s{k} = [data.best_s_active{k}; data.best_s_passive{k}];  %% active + passive objects
+		data.locs{k} = [locations_active{i}(:, f1, :); locations_passive{i}(:, f1, :)];
   elseif isequal(object_type, 'passive')
-    data.best_s{1, k} = [data.best_s_passive{1, k}];  %% passive objects only
-		data.locs{1, k} = [locations_passive{i}(:, f1, :)];
+    data.best_s{k} = [data.best_s_passive{k}];  %% passive objects only
+		data.locs{k} = [locations_passive{i}(:, f1, :)];
   end
   %pause
 end
-
+%keyboard
 % sanity check, should have locations for each score
 assert(isequal(size(data.locs), size(data.best_s)));
 
@@ -69,34 +69,35 @@ for k=1:length(data.label)
   i = data.person(k);
 	
 	% set the start and end frames of the current clip, used in compute_hist
-	num_frames = length(data.frs{1,k});
-	dim.start_frame = data.frs{1,k}(1);
-	dim.end_frame = data.frs{1,k}(num_frames);
+	num_frames = length(data.frs{k});
+	dim.start_frame = data.frs{k}(1);
+	dim.end_frame = data.frs{k}(num_frames);
 
 	feats = struct('person', [], 'x', [], 'y', [], 'z', [], 'label',[]);
 	feats_ind = 0;
-	for r = 1:size(data.best_s{1,k}, 1)
-		for c = 1:size(data.best_s{1,k}, 2)
-			if data.best_s{1,k}(r,c) > 0
+
+	for r = 1:size(data.best_s{k}, 1)
+		for c = 1:size(data.best_s{k}, 2)
+			if data.best_s{k}(r,c) > 0
 				feats_ind = feats_ind + 1;
 				
 				% reshape the location from 1x1x4 to 1x4
-				loc = reshape(data.locs{1,k}(r,c,:), 1,4);
+				loc = reshape(data.locs{k}(r,c,:), 1,4);
 				
 				feats.person(feats_ind) = i;
+				% compute the centroid of the bounding box
 				feats.x(feats_ind) = loc(1) + mean([loc(1) loc(3)]);
 				feats.y(feats_ind) = loc(2) + mean([loc(2) loc(4)]);
+				% the frame is the column in the best score matrix
 				feats.z(feats_ind) = c;
+				% the object label is the row in the best score matrix
 				feats.label(feats_ind) = r;
 			end
 		end
 	end
 
-	fs = data.best_s{1,k} > 0;
 	data.feat(:,k) = compute_hist(feats, nlevels, dim);
 end
-
-
 
 data.feat = bsxfun(@rdivide, data.feat, sum(data.feat, 1) + eps); %% normalizing
 
@@ -104,4 +105,4 @@ thr = 0.01;
 data.feat(data.feat > thr) = thr;   %% clipping features
 
 %save('datafile', data)
-train
+train_and_test
