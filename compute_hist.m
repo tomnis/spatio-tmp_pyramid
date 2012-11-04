83a3fbb5bfef933c610dfe7c2bd37472f7509f72
Tomas <tomniscientia@gmail.com>
2012-11-04 14:27:26 -0600

    fixed a minor bug in compute_hist

--- compute_hist.m
+++ compute_hist.m
@@ -1,24 +1,22 @@
 % given a set of features extracted from a clip and a partition scheme
 % compute the feature histogram
 % dim => struct containing start_frame, end_frame, xlen, ylen, protate
-function [hist] = compute_hist(feats, nlevels, dim, partition)
+% cut_eqs => array of structs. 
+function [hist] = compute_hist(feats, nlevels, cut_eqs, dim)
 	assert(nlevels > 0);
 	hist = [];
 	
-	% apply the partition to the features
-	cut_eqs = apply_partition(partition, dim.xlen, dim.ylen, dim.start_frame, dim.end_frame);
-
 	cuts = struct('xcuts', [], 'ycuts', [], 'zcuts', []);
-
 
 	% level 0 is the entire shot
 	for level = 0:nlevels-1
 
 		if level > 0
-			partition_cuts = partition(level);
-			cuts.xcuts = [cuts.xcuts; partition_cuts.xcut_fracs];
-			cuts.ycuts = [cuts.ycuts; partition_cuts.ycut_fracs];
-			cuts.zcuts = [cuts.zcuts; partition_cuts.zcut_fracs];
+			prt_cuts = cut_eqs(level);
+			
+			cuts.xcuts = [cuts.xcuts; prt_cuts.xcuts];
+			cuts.ycuts = [cuts.ycuts; prt_cuts.ycuts];
+			cuts.zcuts = [cuts.zcuts; prt_cuts.zcuts];
 		end
 
 		% compute the histogram for the current level
