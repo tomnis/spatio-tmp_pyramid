1e509a1d951c6a041b24425e1773215617a9eccb
Tomas <tomniscientia@gmail.com>
2012-11-10 22:15:50 -0600

    appear to have fixed the bin function. points that are clearly in different partitions now are assigned proper labels

--- bin.m
+++ bin.m
@@ -1,23 +1,22 @@
 % given a space-time feature f = (x,y,z) and a set of planar cuts, determine 
 % which unique region f is located in by determining where f is relative to each of the cuts
-% TODO need to know the xlen, ylen, start and end frames to compute this result?
-function [bin_label] = bin(f, cuts, dim)
-	assert(isequal(size(f), [1,3]));
+function [bin_label] = bin(f, cut_eqs, dim)
+	assert(isequal(size(f), [1 3]));
 	bin_label = 0;
-	cuts;
-	size(cuts.xcuts);
-	size(cuts.xcuts,1);
+	cut_eqs;
+	size(cut_eqs.xcuts);
+	size(cut_eqs.xcuts,1);
 	% x := 2x or x := 2x +1 as necessary
-	for i=1:size(cuts.xcuts,1)
-		bin_label = 2 * bin_label + compare(f, cuts.xcuts(i,:), dim);
+	for i=1:size(cut_eqs.xcuts,1)
+		bin_label = 2 * bin_label + compare(f, cut_eqs.xcuts(i,:), dim);
 	end
 
-	for i=1:size(cuts.ycuts,1)
-		bin_label = 2 * bin_label + compare(f, cuts.ycuts(i,:), dim);
+	for i=1:size(cut_eqs.ycuts,1)
+		bin_label = 2 * bin_label + compare(f, cut_eqs.ycuts(i,:), dim);
 	end
 	
-	for i=1:size(cuts.zcuts,1)
-		bin_label = 2 * bin_label + compare(f, cuts.zcuts(i,:), dim);
+	for i=1:size(cut_eqs.zcuts,1)
+		bin_label = 2 * bin_label + compare(f, cut_eqs.zcuts(i,:), dim);
 	end
 
 	bin_label;
@@ -25,16 +24,21 @@ end
 
 
 % return 0 if f is closer to the origin, 1 otherwise
+% cut = [a b c d] where ax + by + cz = d is the equation of the cut plane
 function [c] = compare(f, cut, dim)
 	% get normal vector to this cut plane 
 	% for equation of the form ax + by + cz = d, a normal vector is given by [a b c]
-
 	nrm = cut(1:3);
-	
 	% compute the nearest point on the cut plane, then see which point is closer to the origin
-	%  (f . n) / (n . n) * n where n is the normal to the plane and . denotes dot product.	
-	p = dot(f, nrm) / dot(nrm, nrm) * nrm;
 	
+	% p_prime is an arbitrary point on the plane, so use the 
+	% point on the plane that is closest to the origin, which is given by:
+	% [a b c] * d
+
+	p_prime = nrm * cut(4) / norm(nrm);
+	p = f + (dot(nrm, p_prime) - dot(nrm, f)) * nrm;
+
+
 	% TODO verify this is the correct way to represent the origin
 	origin = [0 0 dim.start_frame];
 	c = dist(f, origin) >= dist(p, origin);
