6fe898426836fb619363a1393c4409a5d76dd3a8
Tomas <tomniscientia@gmail.com>
2012-11-12 03:06:15 -0600

    changed main to have additional parameter that reflects application of spatial cuts in addition to temporal ones

--- main.m
+++ main.m
@@ -5,10 +5,12 @@ setup
 object_type = 'active_passive'
 %object_type = 'passive'
 num_levels = 2
-protate = 0.8
+protate = 0.0
 show_confn = 0;
 % initially start_frame and end_frame set to dummy values
 dim = struct('start_frame', 1, 'end_frame', 1000, 'xlen', 1280, 'ylen', 960, 'protate', protate);
+spatial_cuts = 0
+
 
 prepare_data
 %data
