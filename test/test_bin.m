a9e355ff1174887300ed6e7055613ab877c7c735
Tomas <tomniscientia@gmail.com>
2012-11-12 01:53:24 -0600

    added more tests to test_bin

--- test/test_bin.m
+++ test/test_bin.m
@@ -17,5 +17,13 @@ cuts.ycuts = get_plane_eq(p1, p2, p3);
 [p1 p2 p3] = get_points([dim.end_frame/2 dim.end_frame/2 0], xlen, ylen, start_frame, end_frame);
 cuts.zcuts = get_plane_eq(p1, p2, p3);
 
-bin([1 1 start_frame], cuts, dim)
-bin([xlen ylen end_frame], cuts, dim)
+assert(bin([1 1 start_frame], cuts, dim) == 0);
+assert(bin([1 1 end_frame], cuts, dim) == 1);
+assert(bin([1 ylen start_frame], cuts, dim) == 2);
+assert(bin([1 ylen end_frame], cuts, dim) == 3);
+assert(bin([xlen 1 start_frame], cuts, dim) == 4);
+assert(bin([xlen 1 end_frame], cuts, dim) == 5);
+assert(bin([xlen ylen start_frame], cuts, dim) == 6);
+assert(bin([xlen ylen end_frame], cuts, dim) == 7);
+
+disp('passed all bin tests');
