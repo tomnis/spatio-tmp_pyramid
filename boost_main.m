54cd3d613b0c4e774c0882f21812fb21268c8369
Tomas <tomniscientia@gmail.com>
2012-11-13 03:16:48 -0600

    just changed some params in boost_main

--- boost_main.m
+++ boost_main.m
@@ -1,17 +1,18 @@
+setup
 load tempfile
 
-pool_size = 20;
+pool_size = 10;
 num_levels = 3;
-protate = 0;
-target_accuracy = .5;
+protate = 1;
+target_accuracy = .45;
 object_type = 'active_passive';
-spatial_cuts = 0;
+spatial_cuts = 1;
 dim = struct('start_frame', 1, 'end_frame', 1000, 'xlen', 1280, 'ylen', 960, 'protate', protate, 'spatial_cuts', spatial_cuts);
 
 % assign the best scores to each clip
 compute_scores
 
+% create the partition pool
 pool = make_pool(pool_size, num_levels, protate);
-
 
 f = boost(data, pool, target_accuracy, num_levels, dim);
