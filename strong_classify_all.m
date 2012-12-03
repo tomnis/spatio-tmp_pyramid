0e8d5d685703a07f39f33c4e3a812cd6c7e43a0b
Tomas <tomniscientia@gmail.com>
2012-12-03 04:10:24 -0600

    changed the interface of strong_classify_all to also pass the partitioned data using all relevant partition patterns

--- strong_classify_all.m
+++ strong_classify_all.m
@@ -1,9 +1,10 @@
 % classify all the data points
 % pass in the new labels that have been map1'ed
-function [labels] = strong_classify_all(alpha, min_class_classifiers, data, new_labels)
+% pass in all partitioned features
+function [labels] = strong_classify_all(alpha, min_class_classifiers, min_pat_inds, partitioned_feats, new_labels)
 	labels = [];
-	for i = 1:length(data.label)
-		labels(i) = strong_classify(alpha, min_class_classifiers, data, i, new_labels(i));
+	for i = 1:length(new_labels)
+		labels(i) = strong_classify(alpha, min_class_classifiers, min_pat_inds, partitioned_feats, i, new_labels(i));
 	end
 end
 
@@ -12,16 +13,17 @@ end
 % strongly classify the ith data point
 % = argmax_c (sum_{m=1}^j alpha(m) * f_m(I) == c)
 % j = length(alpha)
-function [label] = strong_classify(alpha, min_class_classifiers, data, i ,new_label)
+% feats
+function [label] = strong_classify(alpha, min_class_classifiers, min_pat_inds, partitioned_feats, i ,new_label)
 	cur_max_label = 0;
 	cur_max_score = 0;
 	
-	num_labels = length(unique(data.label));
+	num_labels = length(unique(new_label));
 
 	for j=1:num_labels
 		cur_score = 0;
 		for m=1:length(alpha)
-			x_test = data.feat(:,i);
+			x_test = partitioned_feats{min_pat_inds(m)}(:,i);
 			y_test = new_label;
 			
 			cur_label = svmpredict(y_test', x_test', min_class_classifiers(m));
