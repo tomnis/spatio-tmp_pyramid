4c745193bc2a16a164bac7b05d847f80166a8f49
Tomas <tomniscientia@gmail.com>
2012-12-10 01:54:01 -0600

    some work on visualization. need to figure out how to represent eqs that cannot be solved for z

---
+++ vis/vis_main.m
@@ -0,0 +1,18 @@
+pool_size = 1;
+num_levels = 3;
+protate = 0;
+spatial_cuts = 1;
+
+% create the partition pool
+pool = make_pool(pool_size, num_levels, protate);
+
+eqs_to_plot = visualize_partition(pool{1}, spatial_cuts);
+
+%{
+ezmesh(eqs_to_plot(1), 400)
+colormap([0 0 1])
+
+
+fh = @(x,y) x.*exp(-x.^2-y.^2);
+ezmesh(fh,40)
+%}
--- vis/visualize_partition.m
+++ vis/visualize_partition.m
@@ -1,21 +1,37 @@
 % given a partition, plot all the planes
-function [] = visualize_partition(partition, spatial_cuts)
-
-% apply the partition to a dummy space
-cut_eqs = apply_partition(partition, 1280, 960, 1, 1000, spatial_cuts);
-
-% we want an array of plane functions to plot
-eqs_to_plot = [];
-syms x y z;
-
-for lvl = 1:length(cut_eqs)
-	level_eqs = cut_eqs(lvl);
-
-	for c = 1:length(level_eqs)
+function [eqs_to_plot] = visualize_partition(partition, spatial_cuts)
+	partition
 
 
+	% apply the partition to a dummy space
+	cut_eqs = apply_partition(partition, 1280, 960, 1, 1000, spatial_cuts);
 
+	% we want an array of plane functions to plot
+	eqs_to_plot = [];
+
+	cut_eqs
+
+	for lvl = 1:length(cut_eqs)
+		level_eqs = cut_eqs(lvl)
+		
+		for c = 1:size(level_eqs.xcuts,1)
+			xcut = level_eqs.xcuts(c,:);
+			ycut = level_eqs.ycuts(c,:);
+			zcut = level_eqs.zcuts(c,:);
+
+			%eqs_to_plot = [eqs_to_plot; get_plane(xcut)];
+			%eqs_to_plot = [eqs_to_plot; get_plane(ycut)];
+			eqs_to_plot = [eqs_to_plot; get_plane(zcut)];
+		end
+	end
 end
 
 
-function [zplane] = get_zplane(cut
+% cut is a vector representing ax + by + cz = d
+% symbolically solve this for z
+function [plane] = get_plane(cut)
+	syms x y z;
+	p = [x, y, z];
+	pfunc = cut(1) * x + cut(2) * y + cut(3) * z - cut(4)
+	plane = solve(pfunc, z);
+end
