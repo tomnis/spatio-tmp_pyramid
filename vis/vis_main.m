cba53091df85be1dc73978f58dba44af58aa1026
Tomas <tomniscientia@gmail.com>
2012-12-10 22:53:55 -0600

    some more work on visualization:

--- vis/vis_main.m
+++ vis/vis_main.m
@@ -8,10 +8,10 @@ pool = make_pool(pool_size, num_levels, protate);
 
 eqs_to_plot = visualize_partition(pool{1}, spatial_cuts);
 
-%{
 ezmesh(eqs_to_plot(1), 400)
 colormap([0 0 1])
 
+%{
 
 fh = @(x,y) x.*exp(-x.^2-y.^2);
 ezmesh(fh,40)
