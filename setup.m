d47845084017cf90bc7ad3e4f26ee542702dcbce
Tomas <tomniscientia@gmail.com>
2012-12-12 15:47:46 -0600

    adding addpath test to setup

--- setup.m
+++ setup.m
@@ -2,6 +2,7 @@ w = whos
 
 addpath /u/tomas/thesis  %% root directory
 addpath /u/tomas/thesis/partition
+addpath /u/tomas/thesis/test
 addpath /u/tomas/thesis/vis 
 addpath /u/tomas/thesis/third_party/libsvm-mat-3.0-1
 
