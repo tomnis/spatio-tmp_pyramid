5024bd780690646fec1136d35a21e564be141c38
Tomas <tomniscientia@gmail.com>
2012-12-13 02:04:28 -0600

    changed params in main and in adl/main

--- main.m
+++ main.m
@@ -1,18 +1,20 @@
 clear
 
 setup
-
-object_type = 'active_passive'
-%object_type = 'passive'
-num_levels = 2
+load loaded_data
+%object_type = 'active_passive'
+object_type = 'passive'
+num_levels = 1
 protate = 0.0
 show_confn = 0;
 % initially start_frame and end_frame set to dummy values
 dim = struct('start_frame', 1, 'end_frame', 1000, 'xlen', 1280, 'ylen', 960, 'protate', protate);
-spatial_cuts = 0
+dim.spatial_cuts = 1;
 
+dataset = DataSet(data, frs, best_scores, locations, object_type);
+pool = make_pool(1, num_levels, protate);
+partition = pool{1};
 
-prepare_data
-%data
-%save('datafile', data)
-accuracy = train_and_test(data, person_ids, show_confn);
+histograms = dataset.compute_histograms(partition, dim);
+
+accuracy = train_and_test(dataset, histograms, person_ids, show_confn);
