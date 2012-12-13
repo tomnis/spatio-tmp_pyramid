6d7ecab3dfc10aa652b37cc4494cacbcba0e74c4
Tomas <tomniscientia@gmail.com>
2012-12-13 02:01:20 -0600

    changed train and test to follow the new interface

--- train_and_test.m
+++ train_and_test.m
@@ -1,30 +1,22 @@
-function [accuracy] = train_and_test(data, person_ids, show_confn)
+% leave one out train and test
+function [accuracy] = train_and_test(dataset, histograms, person_ids, show_confn)
 %%% leave one out train and test
-
+% TODO shouldnt have to know about valid labels here
 valid_labels = [1 2 3 4 5 6 9 10 12 13 14 15 17 20 22 23 24 27];
-
-f1 = find(ismember(data.label, valid_labels));
-%data = sub(data, f1, 2);
-
-labels = unique(data.label);
+labels = unique(dataset.label);
 n_label = length(labels);
-clear map1
-map1(labels+1) = [1:n_label]; %% mapping the action labels to a new label set.
-
 assert(n_label == length(valid_labels));
 
 conf = zeros(n_label, n_label);
 
 for left_out = person_ids
-  f1 = find(data.person ~= left_out);
-  x_train = data.feat(:, f1);
-  y_train = data.label(:, f1);
-  y_train = map1(y_train+1);
+  f1 = find(dataset.person ~= left_out);
+  x_train = histograms(:, f1);
+  y_train = dataset.label(:, f1);
   
-  f1 = find(data.person == left_out);
-  x_test = data.feat(:, f1);
-  y_test = data.label(:, f1);
-  y_test = map1(y_test+1);
+  f1 = find(dataset.person == left_out);
+  x_test = histograms(:, f1);
+  y_test = dataset.label(:, f1);
 
   %%% repeat samples to be balanced
   f3 = [];
