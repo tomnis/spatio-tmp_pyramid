77a97ff40f9bb9e372e60cffdbf10185db258f8b
Tomas <tomniscientia@gmail.com>
2012-12-23 14:26:59 -0600

    changed test call to compute_histograms to conform to the interface

--- test/test_dataset.m
+++ test/test_dataset.m
@@ -1,15 +1,43 @@
 clear
 setup
-load tempfile
-best_scores.active = best_s_active;
-best_scores.passive = best_s_passive;
-locations.active = locations_active;
-locations.passive = locations_passive;
+load loaded_data
 
 object_type = 'active_passive';
 
 d = DataSet(data, frs, best_scores, locations, object_type);
 
+
+best_s_active = best_scores.active;
+best_s_passive = best_scores.passive;
+locations_active = locations.active;
+locations_passive = locations.passive;
+
 compute_scores
+
+num_partitions = 1;
+num_levels = 3;
+protate = 0;
+pool = make_pool(num_partitions, num_levels, protate);
+partition = pool{1};
+dim.xlen = 1280;
+dim.ylen = 960;
+spatial_cuts = 1;
+dim.spatial_cuts = spatial_cuts;
+compute_feats
 d
 data
+
+hists = d.compute_histograms(partition, dim);
+
+assert(isequal(d.person, data.person), 'person error');
+assert(isequal(d.fr_start, data.fr_start), 'fr_start error');
+assert(isequal(d.fr_end, data.fr_end), 'fr_end error');
+assert(isequal(d.label, data.label), 'label error');
+assert(isequal(d.frs, data.frs), 'frs error');
+assert(isequal(d.best_s_active, data.best_s_active), 'best_s_active error');
+assert(isequal(d.best_s_passive, data.best_s_passive), 'best_s_passive error');
+assert(isequal(d.best_s, data.best_s), 'best_s error');
+assert(isequal(d.locs, data.locs), 'locs error');
+
+
+assert(isequal(hists, data.feat), 'histogram error');
