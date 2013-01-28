7aae0a64e312891b77d35b7c850fcfa521443b6b
Tomas <tomniscientia@gmail.com>
2013-01-28 15:08:31 -0600

    adding script to generate a bunch of pools. use to fix the set of pools and try different things

---
+++ partition/generate_pools.m
@@ -0,0 +1,32 @@
+% generate a set of partition pools
+%
+% num_pools -> number of pools to generate
+% pool_size -> number of partitions in each pool
+% num_levels -> number of levels in the pyramid
+% prorate -> probability of rotating cut planes
+% bias_Type -> 0, 1,2. unbiased, cut around active objects, cut through active objects
+function pools = generate_pools(num_pools, pool_size, num_levels, protate, bias_type, dataset)
+	regular = 0;
+	
+	
+	% TODO should i compute the distribution on the train, or all data?
+	if bias_type > 1
+		distr = dataset.compute_obj_distrs(10);
+	else
+		distr.bx = [];
+		distr.by = [];
+		distr.bz = [];
+	end
+	
+	if bias_type == 2
+		inverse = 1;
+	else
+		inverse = 0;
+	end
+	randrs.x = RandDistr(distr.bx, inverse);
+	randrs.y = RandDistr(distr.by, inverse);
+	randrs.z = RandDistr(distr.bz, inverse);
+
+	for itr=1:num_pools
+		pools{itr} = make_pool(pool_size, num_levels, protate, regular, randrs);
+	end
