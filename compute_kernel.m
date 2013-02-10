3018bc60a4d97942575c79fb551de1018390792d
Tomas <tomniscientia@gmail.com>
2013-02-10 16:05:41 -0600

    changed compute_kernel to take advantage of symmetric kernel matrix. this happens when the kernel is computed between the training set and itself.

--- compute_kernel.m
+++ compute_kernel.m
@@ -20,6 +20,7 @@ function [K] = chi_sq_dist(hist1, hist2)
 	   K(:,i) = sum(d.^2 ./ (s/2+eps), 2);
 	end
 	K = 1 - K;
+	
 
 
 
@@ -29,8 +30,20 @@ function [K] = hist_intersect(hist1, hist2)
 	kc = size(hist2,1);
 	K = zeros(kr, kc);
 
-	for i=1:size(K,1)
-		for j=1:size(K,2)
-			K(i,j) = sum(min(hist1(i,:), hist2(j,:)));
+
+	if kr == kc
+		
+		for i=1:kr
+			for j=1:i
+				K(i,j) = sum(min(hist1(i,:), hist2(j,:)));
+				K(j,i) = K(i,j);
+			end
+		end
+
+	else
+		for i=1:kr
+			for j=1:kc
+				K(i,j) = sum(min(hist1(i,:), hist2(j,:)));
+			end
 		end
 	end
