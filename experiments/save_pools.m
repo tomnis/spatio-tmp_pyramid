435fe48a4636411d84b4d9ef4919bc775620600a
Tomas <tomniscientia@gmail.com>
2013-02-10 16:19:54 -0600

    changed the values of pool size in save_pools

--- experiments/save_pools.m
+++ experiments/save_pools.m
@@ -4,8 +4,8 @@ load loaded_data
 
 % generate a set of pools. intented to replace the 
 num_pools = 20
-num_levels = 3;
-pool_size = 50;
+num_levels = 2;
+pool_size = 100;
 protate = 0;
 regular = 0;
 
@@ -15,14 +15,25 @@ dataset = DataSet(data, frs, best_scores, locations, object_type);
 
 % set of unbiased pools
 bias_type = 1;
-allpools{bias_type} = generate_pools(num_pools, pool_size, num_levels, protate, bias_type, dataset);
+allpools{bias_type} = generate_pools(num_pools, pool_size, num_levels, protate, bias_type, regular, dataset);
 
 % set of pools to cut through AO region
 bias_type = 2;
-allpools{bias_type} = generate_pools(num_pools, pool_size, num_levels, protate, bias_type, dataset);
+allpools{bias_type} = generate_pools(num_pools, pool_size, num_levels, protate, bias_type, regular, dataset);
 
 % set of pools to cut around AO region
 bias_type = 3;
-allpools{bias_type} = generate_pools(num_pools, pool_size, num_levels, protate, bias_type, dataset);
+allpools{bias_type} = generate_pools(num_pools, pool_size, num_levels, protate, bias_type, regular, dataset);
 
-save('allpools.mat', 'allpools')
+%{
+for bias_type =1:3
+	clear pools
+	pools = generate_pools(1, 1, 1, 0, bias_type, 0, dataset)
+	for num_levels = 2:3
+
+	end
+	allpools{bias_type} = pools;
+end
+%}
+
+save('allpoolslvls2-3size100.mat', 'allpools')
