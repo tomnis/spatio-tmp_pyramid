0a7dc72104e03cbddcf256b4fb8a7e22e8eae7f9
Tomas <tomniscientia@gmail.com>
2013-02-28 02:41:47 -0600

    changed leave one out to conform to new boosting

--- leave_one_out_main.m
+++ leave_one_out_main.m
@@ -3,15 +3,15 @@ load loaded_data
 object_type = 'active_passive';
 
 num_levels = 3;
-pool_size = 15;
+pool_size = 50;
 protate = 0;
 regular = 0;
 
 d = DataSet(data, frs, best_scores, locations, object_type);
 
-bias = 1;
-% TODO should i compute the distribution on the train, or all data?
-if bias
+bias_type = 3;
+
+if bias_type > 1
 	distr = d.compute_obj_distrs(10);
 else
 	distr.bx = [];
@@ -19,11 +19,18 @@ else
 	distr.bz = [];
 end
 
-randrs.x = RandDistr(distr.bx);
-randrs.y = RandDistr(distr.by);
-randrs.z = RandDistr(distr.bz);
+if bias_type == 2
+	inverse = 1;
+else
+	inverse = 0;
+end
+randrs.x = RandDistr(distr.bx, inverse);
+randrs.y = RandDistr(distr.by, inverse);
+randrs.z = RandDistr(distr.bz, inverse);
 
 % create the partition pool
 pool = make_pool(pool_size, num_levels, protate, regular, randrs);
 
 [accuracy confn] = leave_one_out(d, pool, person_ids);
+
+save('/u/tomas/thesis/results/leave_one_out/leav_one_out', 'accuracy', 'confn')
