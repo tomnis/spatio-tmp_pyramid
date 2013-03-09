d343f023fa96f1f6dbe4fc867e06c1a61bc5788d
Tomas <tomniscientia@gmail.com>
2013-03-09 15:07:51 -0600

    combine results

--- combine_results.m
+++ combine_results.m
@@ -3,7 +3,16 @@ function [combined_accuracies, all_confns] = combine_results(directory)
 files = dir(directory);
 files = files(3:end);
 
-for i = 1:length(files)
+for i = 1:100
+	if i == 16
+		continue
+	end
+	if i == 52
+		continue
+	end
+	if i == 94
+		continue
+	end
 	load([directory, '/acc_and_confntrial', num2str(i)]);
 	
 	combined_accuracies(i) = accuracies(i)
