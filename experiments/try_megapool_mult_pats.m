0b8e957a32f7d291d3a4290c51e72f85e9d985b5
Tomas <tomniscientia@gmail.com>
2013-03-18 18:44:21 -0500

    removed checkpools reference, added pyramid 3 lvl size 500

--- experiments/try_megapool_mult_pats.m
+++ experiments/try_megapool_mult_pats.m
@@ -5,5 +5,5 @@ load megapool_almost_giga_tpyramids
 % bias type
 allmegapools{1} = megapool;
 allmegapoolss{1} = allmegapools;
-[accuracies, all_confns, fs] = run_experiment_checkpools_mult_pats(allmegapoolss, 1, 'poly', trial_num+1);
+[accuracies, all_confns, fs] = run_experiment_mult_pats(allmegapoolss, 1, 'poly', trial_num+1);
 save(['/u/tomas/thesis/results/almost_gigapool_mult_pats/acc_and_confntrial', num2str(trial_num+1)], 'accuracies', 'all_confns', 'fs');
