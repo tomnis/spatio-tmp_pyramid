6cc381e011ccfb93912ab7227ebf8e4c9040a0e9
Tomas <tomniscientia@gmail.com>
2013-04-03 01:27:49 -0500

    remove some debug from boost_varyrounds

--- boost/boost_varyrounds.m
+++ boost/boost_varyrounds.m
@@ -1,11 +1,11 @@
 % data contains labels
 % output is strong classifier
-function [f] = boost_varyrounds(dataset, pool, target_accuracy, dim, kernel_type, boost_rounds)
+function [f] = boost_varyrounds(dataset, pool, target_accuracy, dim, kernel_type, num_rounds)
   spatial_cuts = dim.spatial_cuts;
 
   labels = unique(dataset.label);
   n_label = length(labels);
- 
+
   % for each partition pattern
   for prt_num = 1:length(pool)
 		fprintf(1, '\rpattern %d of %d...', prt_num, length(pool));
@@ -18,9 +18,12 @@ function [f] = boost_varyrounds(dataset, pool, target_accuracy, dim, kernel_type
   	partitioned_feats{prt_num} = hists;
   
   	% randomly sample a subset of the clips
-		num_sample_clips = randi(dataset.num_clips);
-  	sampleinds = sort(randsample(dataset.num_clips, num_sample_clips));
-  
+		%num_sample_clips = randi(dataset.num_clips);
+  	%sampleinds = sort(randsample(dataset.num_clips, num_sample_clips));
+  	% changed 2/6 4pm:
+		% remove the randomness from training on random subsets
+		sampleinds = 1:dataset.num_clips;
+
   	% train an svm classifier on the subset
   	x_train = hists(:, sampleinds);
   	y_train = dataset.label(sampleinds);
@@ -28,27 +31,28 @@ function [f] = boost_varyrounds(dataset, pool, target_accuracy, dim, kernel_type
     %%% repeat samples to be balanced
     f3 = [];
     for j = 1:n_label
-      f1 = find(y_train == j);
+      l = labels(j);
+      f1 = find(y_train == l);
       f1_n = length(f1);
       if f1_n == 0
         continue
       end
       
-      f2 = repmat(f1, [1 ceil(100/f1_n)]);
-      f3 = [f3 f2(1:100)];
+      f2 = repmat(f1, [1 ceil(50/f1_n)]);
+      f3 = [f3 f2(1:50)];
     end
   
   	% select the training examples to use
     x_train1 = x_train(:, f3);
     y_train1 = y_train(:, f3);
- 
+
 		if isequal(kernel_type, 'poly')
- 			fprintf(1, ' training with polynomial kernel...');
+ 			fprintf(1, ' training... (polynomial kernel,  ex:%d) ', length(y_train1));
  		  svm = svmtrain(y_train1', x_train1', '-c 1 -t 0 -q');
 		else 
 			fprintf(1, ' precomputing %s kernel...', kernel_type);
 			K = compute_kernel(x_train1', x_train1', kernel_type);
- 			fprintf(1, ' training...');
+ 			fprintf(1, ' training... (ex:%d)', length(y_train1));
 			trains{prt_num} = x_train1;
 			svm = svmtrain(y_train1', K, '-q -t 4');
 		end
@@ -71,7 +75,7 @@ function [f] = boost_varyrounds(dataset, pool, target_accuracy, dim, kernel_type
   j = 0;
   accuracy = 0;
   accuracies = [];
-  while accuracy < target_accuracy && j < boost_rounds
+  while accuracy < target_accuracy && j < num_rounds
   	
   	% for each clip, update the weight
   	weights = weights ./ sum(weights);
@@ -86,24 +90,30 @@ function [f] = boost_varyrounds(dataset, pool, target_accuracy, dim, kernel_type
   	min_pat_ind = 1;
   	for pattern_ind = 1:length(pool)
   		x_test = partitioned_feats{pattern_ind}';
-  		
-			
-			if isequal(kernel_type, 'poly')
-  			y_pred = svmpredict(y_test, x_test, classifiers(pattern_ind));
+  	
+			% first boosting round we compute the and cache the indicators
+			if j == 1
+				if isequal(kernel_type, 'poly')
+					y_pred = svmpredict(y_test, x_test, classifiers(pattern_ind));
+				else
+					xtest = compute_kernel(x_test, trains{pattern_ind}', kernel_type);
+					y_pred = svmpredict(y_test, xtest, classifiers(pattern_ind));
+				end
+				
+				indicator = y_pred' ~= dataset.label;
+				cur_err = dot(weights, indicator);
+				
+				% cache the indicator to be used in later iterations
+				indicators{pattern_ind} = indicator;	
 			else
-				xtest = compute_kernel(x_test, trains{pattern_ind}', kernel_type);
-				y_pred = svmpredict(y_test, xtest, classifiers(pattern_ind));
+				cur_err = dot(weights, indicators{pattern_ind});
 			end
-  		
-			indicator = y_pred' ~= dataset.label;
-  		
-  		cur_err = dot(weights, indicator);
-  		
+
   		% if we find a new minimum, store the error, pattern, and indicator
   		if cur_err <= min_err
   			min_err = cur_err;
   			min_pat_ind = pattern_ind;
-  			min_indicator = indicator;
+  			min_indicator = indicators{pattern_ind};
   		end
   	end
   
@@ -122,10 +132,10 @@ function [f] = boost_varyrounds(dataset, pool, target_accuracy, dim, kernel_type
   	end
   
   	% generate the strong classifier
-  	strong_classifications = strong_classify_all(f, partitioned_feats, dataset.valid_labels);
+  	%strong_classifications = strong_classify_all(f, partitioned_feats, dataset.valid_labels);
 		% compute its classification accuracy (percentage of correct classifications)
-  	accuracy = mean(strong_classifications == dataset.label);	
-  	f.accuracies(j) = accuracy;
+  	%accuracy = mean(strong_classifications == dataset.label);	
+  	%f.accuracies(j) = accuracy;
   end
-	f.accuracies;
+	%f.accuracies
 end
