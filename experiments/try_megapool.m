2afa5e649d9ce7f1b709ddde5e380ec309f5ae4a
Tomas <tomniscientia@gmail.com>
2013-04-03 01:31:51 -0500

    moved megapool stuff to appropriate script folders, removed leave one out stuf with bias references that we dont need anymore

--- experiments/try_megapool.m
+++
@@ -1,9 +0,0 @@
-function [] = try_megapool(trial_num)
-load allpyramidslvl4size100
-
-% a bit of dirty hacking.. run experiment expects an extra level of nesting because of 
-% bias type
-allmegapools{1} = megapool;
-allmegapoolss{1} = allmegapools;
-[accuracies, all_confns, fs] = run_experiment_checkpools(allmegapoolss, 1, 'poly', trial_num+1);
-save(['/u/tomas/thesis/results/pyramidslvl4size100/acc_and_confntrial', num2str(trial_num+1)], 'accuracies', 'all_confns', 'fs');
