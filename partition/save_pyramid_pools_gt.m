7c90f3620306b8e6d6a3b2c40358296568396073
Tomas <tomniscientia@gmail.com>
2013-04-12 15:57:08 -0500

    methods to compute biased pools for ga and gt

--- partition/save_pyramid_pools_gt.m
+++ partition/save_pyramid_pools_gt.m
@@ -1,13 +1,13 @@
 setup
-load loaded_gatech
+load loaded_data_gt
 
 num_pools = 1
-num_levels = 2
+num_levels = 4
 pool_size = 100
 protate = 0;
 regular = 0;
 
-object_type = 'active';
+object_type = 'active_passive';
 dataset = DataSet(data, frs, best_scores, locations, object_type);
 
 perm = [1,3,2];
@@ -21,4 +21,4 @@ end
 pstr = num2str(perm);
 pstr(ismember(pstr, ' ')) = [];
 
-save(['allpyramidslvl', num2str(num_levels), 'size', num2str(pool_size), 'perm', pstr, 'ga.mat'], 'allpyramids');
+save(['allpyramidslvl', num2str(num_levels), 'size', num2str(pool_size), 'perm', pstr, 'gt.mat'], 'allpyramids');
